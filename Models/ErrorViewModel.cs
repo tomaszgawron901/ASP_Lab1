43f62bb5721e99d8cec56c08917fb42f522e81bf
tomaszgawron901 <tomasz.gawron@microsoft.wsei.edu.pl>
2020-10-08 10:05:46 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace testingMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
