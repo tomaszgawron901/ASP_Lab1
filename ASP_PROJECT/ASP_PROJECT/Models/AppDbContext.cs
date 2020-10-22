c22df004fb2e956bbe3fa5c31ad01a3f1c2299ef
tomaszgawron901 <tomasz.gawron@microsoft.wsei.edu.pl>
2020-10-22 17:42:53 +0200

    lab2 part2

---
+++ ASP_PROJECT/ASP_PROJECT/Models/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP_PROJECT.Models
+{
+    public class AppDbContext: DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public AppDbContext(DbContextOptions<AppDbContext> options): base() { }
+    }
+}
--- ASP_PROJECT/ASP_PROJECT/Startup.cs
+++ ASP_PROJECT/ASP_PROJECT/Startup.cs
@@ -6,6 +6,8 @@ using ASP_PROJECT.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -13,12 +15,18 @@ namespace ASP_PROJECT
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            this.Configuration = configuration;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
-            services.AddTransient<IProductRepository, FakeProductRepository>();
+            services.AddTransient<IProductRepository, EFProductRepository>();
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration["Data:SportStoreProducts:ConnectionString"]));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
